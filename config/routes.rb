25d6dbd4b64a671bc46f69a1190f9d9a355a10bd
karolcircle <serwisnet1@gmail.com>
2017-05-18 22:07:54 +0200

    Add google account login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+ 
   root to: 'books#index'
 
   resources :lendings do
